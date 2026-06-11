47f76b766ff6c570e7c21d8f1b0c17682616e1e3
User Name <user@example.com>
2026-06-11 18:20:56 +0300

    Implement Song class with class attributes and methods for music library system

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment total count
+        Song.count += 1
+        
+        # Add to genres set
+        Song.genres.add(genre)
+        
+        # Add to artists set
+        Song.artists.add(artist)
+        
+        # Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        
+        # Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
